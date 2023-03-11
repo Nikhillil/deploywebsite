c6c25cb571dd42a594aabe55353f5606f87693f1
nikhil <nikhillilwani999@gmail.com>
2023-03-11 17:38:25 +0530

    initial

---
+++ router.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+const path = require("path");
+
+const pathdir = path.join(__dirname, "public");
+
+
+router.get("/", (req, res) => {
+    res.sendFile(`${pathdir}/index.html`);
+})
+
+module.exports = router;
